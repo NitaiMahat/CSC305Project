ba6a6c703ccb1af136ae890397e5f7cc78b4d2ea
Ethan Herstedt <ethan.herstdt24@augustana.edu>
2025-09-29 23:23:09 -0500

    stated team class with basic constructors

--- src/main/java/edu/augustana/csc305/project/model/Team.java
+++ src/main/java/edu/augustana/csc305/project/model/Team.java
@@ -1,4 +1,22 @@
 package edu.augustana.csc305.project.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
+
+    private String teamName;
+    private List<String> players;
+
+    public Team(String teamName){
+        this.teamName = teamName;
+        this.players = new ArrayList<>();
+    }
+
+    public Team(String teamName, List<String> players){
+        this(teamName);
+        if (players != null){
+            this.players.addAll(players);
+        }
+    }
 }
